e4aea06bbd9509be8c50119fac07148847e291b0
Sergii <seregaonma@gmail.com>
2023-11-26 04:32:26 +0200

    create function for adding class-first to required elements

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 const liLast = document.querySelectorAll('ul > li:last-child');
 liLast.forEach((li) => li.classList.add('last'));
+
+const root = document.querySelector('ul.root');
+
+const setFirstItemClassName = function(level, ul) {
+  level--;
+  if(level > 0) {
+    const liInitial = [...ul.children];
+    liInitial.forEach(li => {
+      const innerUl = [...li.children];
+      innerUl.forEach(ul => setFirstItemClassName(level, ul));
+    })
+  } else {
+    const liFirst = ul.querySelector('li:first-of-type');
+    liFirst.classList.add('first');
+  }
+}
+setFirstItemClassName(2, root);
